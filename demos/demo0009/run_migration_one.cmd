3ff83bff15ec4b1b676b7c53f8815b16b3e1a9f9
Gerd Volberg <github@volberg.net>
2017-07-05 07:10:54 +0200

    Upload Demo0009

---
+++ demos/demo0009/run_migration_one.cmd
@@ -0,0 +1,3 @@
+K:
+CD K:\kassandra\migration\batch\00addon
+"C:\Oracle\FormsAPI_v3_64bit\FapiMaster.exe" /SCRIPT /API=12c /RUN="K:\kassandra\migration\batch\skripte\mig_main_one.p2s" 
---
+++ demos/demo0009/run_win2unix.cmd
@@ -0,0 +1,2 @@
+SET FORMS_PATH=K:\kassandra\migration\batch\00addon
+"C:\Oracle\FormsAPI_v3_64bit\FapiMaster.exe" /SCRIPT /API=12c /RUN="K:\kassandra\migration\batch\skripte\mig_Win2Unix_one.p2s" 
