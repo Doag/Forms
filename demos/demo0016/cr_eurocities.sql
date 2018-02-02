5a835cdd95eb7c6c9db85ea0911a45ce79b21056
Gerd Volberg <github@volberg.net>
2018-02-02 08:39:41 +0100

    Update Autocomplete und Key-Down

---
+++ demos/demo0016/cr_eurocities.sql
@@ -0,0 +1,96 @@
+--
+-- Demo : Autocomplete Combo Box
+-- Friedhold Matz, 16.09.2007
+--
+create table europecities( 
+ name varchar2 (30)
+ constraint pk_europecities primary key  
+)
+/
+	
+
+		insert into europecities values ('Berlin');
+		insert into europecities values ('Paris');
+		insert into europecities values ('Copenhagen');
+		insert into europecities values ('Hamburg');
+		insert into europecities values ('Stockholm');
+		insert into europecities values ('Amsterdam');
+		insert into europecities values ('Dublin');
+		insert into europecities values ('Rome');
+		insert into europecities values ('Madrid');
+		insert into europecities values ('Milan');
+		insert into europecities values ('Barcelona');
+		insert into europecities values ('Valencia');
+		insert into europecities values ('Padua');
+		insert into europecities values ('Vienna');
+		insert into europecities values ('Budapest');
+		insert into europecities values ('Luxembourg');
+		insert into europecities values ('Bruessels');
+		insert into europecities values ('Oslo');
+		insert into europecities values ('Bratislava');
+		insert into europecities values ('Granada');
+		insert into europecities values ('St. Petersburg');
+		insert into europecities values ('Prague');
+		insert into europecities values ('Warsaw');
+		insert into europecities values ('Lisbon');
+		insert into europecities values ('Florence');
+		insert into europecities values ('Venice');
+		insert into europecities values ('Sevillia');
+		insert into europecities values ('Marseille');
+		insert into europecities values ('Naples');
+		insert into europecities values ('Cork');
+		insert into europecities values ('Limerick');
+		insert into europecities values ('Edinburgh');
+		insert into europecities values ('London');
+		insert into europecities values ('Moscow');
+		insert into europecities values ('Kiev');
+		insert into europecities values ('Malaga');
+		insert into europecities values ('Toledo');
+		insert into europecities values ('Bilbao');
+		insert into europecities values ('Nizza');
+		insert into europecities values ('Toulouse');
+		insert into europecities values ('Bordeaux');
+		insert into europecities values ('Strassburg');
+		insert into europecities values ('Dresden');
+		insert into europecities values ('Cologne');
+		insert into europecities values ('Mannheim');
+		insert into europecities values ('Nuremberg');
+		insert into europecities values ('Zurich');
+		insert into europecities values ('Bern');
+		insert into europecities values ('Geneva');
+		insert into europecities values ('Utrecht');
+		insert into europecities values ('Leipzig');
+		insert into europecities values ('Stuttgart');
+		insert into europecities values ('Linz');
+		insert into europecities values ('Salzburg');
+		insert into europecities values ('Wrozlaw');
+		insert into europecities values ('Spremberg');
+		insert into europecities values ('Cottbus');
+		insert into europecities values ('Faro');
+		insert into europecities values ('Albufeira');
+		insert into europecities values ('Gibraltar');
+		insert into europecities values ('Porto');
+		insert into europecities values ('Ennis');
+		insert into europecities values ('Galway');
+		insert into europecities values ('Belfast');
+		insert into europecities values ('Lille');
+		insert into europecities values ('Tuebingen');
+		insert into europecities values ('Heidelberg');
+		insert into europecities values ('Wuerzburg');
+		insert into europecities values ('Munich');
+		insert into europecities values ('Freiburg');
+		insert into europecities values ('Lyon');
+		insert into europecities values ('Liverpool');
+		insert into europecities values ('Birmingham');
+		insert into europecities values ('Glasgow');
+		insert into europecities values ('Helsinki');
+		insert into europecities values ('San Sebastian');
+		insert into europecities values ('Palermo');
+		insert into europecities values ('Krakow');
+		insert into europecities values ('Tallinn');
+		insert into europecities values ('Riga');
+		insert into europecities values ('Vilnius');
+		insert into europecities values ('Belgrade');
+
+COMMIT;
+
